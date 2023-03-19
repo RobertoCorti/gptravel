2096846d974229464bf82f479b3d964f63c1a25f
Roberto Corti <robi290596@gmail.com>
2023-03-19 18:53:04 +0100

    added wiki summary to travel plan + check on dates to app.js

--- src/gptravel/app/app.py
+++ src/gptravel/app/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pycountry
+import wikipediaapi
+
+wiki_wiki = wikipediaapi.Wikipedia('en')
 
 app = Flask(__name__)
 
@@ -6,20 +10,55 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return redirect(url_for('travel_plan',
-                                country=request.form['country'],
-                                departure_date=request.form['departure_date'],
-                                return_date=request.form['return_date']))
+        country = request.form['country']
+        departure_date = request.form['departure_date']
+        return_date = request.form['return_date']
+        region = request.form.get('region')
+
+        if region:
+            return redirect(url_for('travel_plan',
+                                    country=country,
+                                    departure_date=departure_date,
+                                    return_date=return_date,
+                                    region=region))
+        else:
+            return redirect(url_for('travel_plan',
+                                    country=country,
+                                    departure_date=departure_date,
+                                    return_date=return_date))
     else:
         return render_template('index.html')
 
 
 @app.route('/travel_plan/<country>/<departure_date>/<return_date>')
-def travel_plan(country, departure_date, return_date):
+@app.route('/travel_plan/<country>/<departure_date>/<return_date>/<region>')
+def travel_plan(country, departure_date, return_date, region=None):
+    wiki_summary = None
+    try:
+        country_name = pycountry.countries.get(alpha_2=country).name
+    except AttributeError:
+        country_name = country
+    if region:
+        # get region page
+        region_page = wiki_wiki.page(region)
+        if region_page.exists():
+            wiki_summary = region_page.summary
+    else:
+        # get country page from wikipedia
+        country_page = wiki_wiki.page(country_name)
+
+        if country_page.exists():
+            wiki_summary = country_page.summary
+
+    if wiki_summary is not None:
+        wiki_summary = '.'.join(wiki_summary.split('.')[:2])
+
     return render_template('travel_plan.html',
-                           country=country,
+                           country=country_name,
                            departure_date=departure_date,
-                           return_date=return_date)
+                           return_date=return_date,
+                           region=region,
+                           summary=wiki_summary)
 
 
 if __name__ == "__main__":
--- src/gptravel/app/static/app.js
+++ src/gptravel/app/static/app.js
@@ -1,22 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const countryDropdown = document.querySelector('#country');
+  const regionDropdown = document.querySelector('#region');
+  const departureDateInput = document.getElementById("departure_date");
+  const returnDateInput = document.getElementById("return_date");
 
-  const selectDrop = document.querySelector('#country');
-  // const selectDrop = document.getElementById('countries');
 
-
-  fetch('https://restcountries.com/v3.1/all').then(res => {
-    return res.json();
-  }).then(data => {
-    let output = "<option disabled selected value>Select a Country </option>";
-    data.forEach(country => {
-      output += `
-      
-      <option value="${country.altSpellings[0]}">${country.name.common}</option>`;
+  fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => {
+      const sortedData = data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+      let countryOutput = "<option disabled selected value>Select a Country</option>";
+      sortedData.forEach(country => {
+        countryOutput += `
+          <option value="${country.altSpellings[0]}">${country.name.common}</option>
+        `;
+      })
+      countryDropdown.innerHTML = countryOutput;
     })
-    selectDrop.innerHTML = output;
-  }).catch(err => {
-    console.log(err);
-  })
+    .catch(err => console.error(err));
 
-
+  countryDropdown.addEventListener('change', () => {
+    const selectedCountryCode = countryDropdown.value;
+    if (selectedCountryCode !== '') {
+      fetch(`http://api.geonames.org/searchJSON?country=${selectedCountryCode}&featureCode=ADM1&username=robcorti`)
+        .then(res => res.json())
+          .then(data => {
+            const regions = data.geonames.sort((a, b) => a.name.localeCompare(b.name));
+            let regionOutput = "<option disabled selected value>Select a Region</option>";
+            regions.forEach(region => {
+              regionOutput += `
+               <option value="${region.name}">${region.name}</option>
+            `;
+          })
+          regionDropdown.innerHTML = regionOutput;
+        })
+        .catch(err => console.error(err));
+    } else {
+      regionDropdown.innerHTML = "<option disabled selected value>Select a Region</option>";
+    }
+  });
+  // Check if departure date is before return date
+  returnDateInput.addEventListener('change', event => {
+    // check if value is not empty
+    if (returnDateInput.value !== '' && departureDateInput.value !== '') {
+        if (departureDateInput.value > returnDateInput.value) {
+            alert('Return date must be after departure date');
+            returnDateInput.value = departureDateInput.value;
+        }
+    }
+  });
 });
+
