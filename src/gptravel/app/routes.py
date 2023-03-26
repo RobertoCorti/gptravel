b11bef4b1601f0759ae6455463e0be775cc1ac3c
Roberto Corti <robi290596@gmail.com>
2023-03-26 12:35:42 +0200

    started map embedding and slideshow for itinerary

--- src/gptravel/app/routes.py
+++ src/gptravel/app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, request, redirect, url_for
 import pycountry
 import wikipediaapi
+
+from geopy.geocoders.base import Geocoder
+from src.gptravel.app.geo import geolocator
 from src.gptravel.app import app
 
 wiki_wiki = wikipediaapi.Wikipedia('en')
@@ -16,6 +19,20 @@ def _get_travel_plan():
             "Florence": ["Visit Uffizi", "Walk in the city-center"]
         }
     }
+
+
+def get_wikipedia_summary(destination: str) -> str:
+    wiki_page = wiki_wiki.page(destination)
+    wiki_summary = wiki_page.summary
+    return '.'.join(wiki_summary.split('.')[:3]) if wiki_page.exists() else None
+
+
+def get_markers_coordinates(travel_plan_dict: dict, geocoder: Geocoder) -> list:
+    markers_coordinates = []
+    for day in travel_plan_dict:
+        for city in travel_plan_dict[day]:
+            markers_coordinates.append([geocoder.geocode(city).latitude, geocoder.geocode(city).longitude])
+    return markers_coordinates
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -43,35 +60,30 @@ def index():
 
 @app.route('/travel_plan/<country>/<departure_date>/<return_date>')
 @app.route('/travel_plan/<country>/<departure_date>/<return_date>/<region>')
-def travel_plan(country, departure_date, return_date, region=None):
-    wiki_summary = None
+def travel_plan(country: str, departure_date: str, return_date: str, region: str = None):
     try:
         country_name = pycountry.countries.get(alpha_2=country).name
     except AttributeError:
         country_name = country
-    if region:
-        # get region page
-        region_page = wiki_wiki.page(region)
-        if region_page.exists():
-            wiki_summary = region_page.summary
-    else:
-        # get country page from wikipedia
-        country_page = wiki_wiki.page(country_name)
 
-        if country_page.exists():
-            wiki_summary = country_page.summary
+    destination = region if region else country_name
+    wiki_summary = get_wikipedia_summary(destination)
+
+    destination_coordinates = geolocator.geocode(destination).latitude, geolocator.geocode(destination).longitude
 
     travel_plan_json = _get_travel_plan()
 
-    if wiki_summary is not None:
-        wiki_summary = '.'.join(wiki_summary.split('.')[:3])
+    markers_coordinates = get_markers_coordinates(travel_plan_json, geolocator)
 
     return render_template(
         'travel_plan.html',
+        destination=destination,
         departure_date=departure_date,
         return_date=return_date,
         summary=wiki_summary,
-        travel_plan=travel_plan_json
+        travel_plan=travel_plan_json,
+        destination_coordinates=destination_coordinates,
+        markers_coordinates=markers_coordinates
     )
 
 
--- src/gptravel/app/static/js/travel_plan.js
+++ src/gptravel/app/static/js/travel_plan.js
@@ -1,7 +1,39 @@
-$(document).ready(function() {
-  // When the travel itinerary title is clicked
-  $('.travel-itinerary-title').click(function() {
-    // Toggle the open class on the travel itinerary list
-    $(this).next('.travel-itinerary-list').toggleClass('open');
-  });
-});
+var map = L.map('map').setView(geoCoordinates, 5);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+// Creating markers
+for (let i = 0; i < markerCoordinates.length; i++) {
+    var marker = L.marker(markerCoordinates[i]).addTo(map);
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
