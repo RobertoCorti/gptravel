1934358edcade22a55e614374b373882b08e048e
Stefano Polo <sp.stefano.polo@gmail.com>
2023-04-03 22:22:37 +0200

    add TSP solver for travel plan distance optimality

---
+++ src/gptravel/core/services/engine/tsp_solver.py
@@ -0,0 +1,41 @@
+from typing import List, Tuple
+
+import numpy as np
+from python_tsp.exact import solve_tsp_dynamic_programming
+from python_tsp.heuristics import solve_tsp_simulated_annealing
+
+from gptravel.core.services.geocoder import GeoCoder
+
+
+class TSPSolver:
+    def __init__(self, geocoder: GeoCoder) -> None:
+        self._geocoder = geocoder
+
+    def solve(
+        self, cities: List[str], open_problem: bool = False
+    ) -> Tuple[List[str], float]:
+        if len(cities) > 1:
+            solver = (
+                solve_tsp_dynamic_programming
+                if len(cities) < 10
+                else solve_tsp_simulated_annealing
+            )
+            distance_matrix = np.array(
+                [
+                    [
+                        0.0
+                        if i == j
+                        else self._geocoder.location_distance(cities[i], cities[j])
+                        for j in range(len(cities))
+                    ]
+                    for i in range(len(cities))
+                ]
+            )
+            if open_problem:
+                distance_matrix[:, 0] = 0.0
+            solution_indexes, solution_distance = solver(
+                distance_matrix=distance_matrix
+            )
+            return [cities[index] for index in solution_indexes], solution_distance
+        else:
+            return cities, 0.0
--- tests/test_gptravel/test_core/test_services/test_geocoder.py
+++ tests/test_gptravel/test_core/test_services/test_geocoder.py
@@ -14,9 +14,20 @@ class TestGeoCoder:
         assert geo_coder.country_from_location_name("PortaSigrar") is None
 
     def test_location_coordinates(self, geo_coder: GeoCoder):
-        assert geo_coder.location_coordinates("kolkata") == {"lat": 22.5726459, "lon": 88.3638953}
-        assert geo_coder.location_coordinates("delhi") == {"lat": 28.6517178, "lon": 77.2219388}
-        assert geo_coder.location_coordinates("PortaSigrar") == {"lat": None, "lon": None}
-    
-    def test_location_distance(self, geo_coder: GeoCoder): 
-        assert geo_coder.location_distance("kolkata", "delhi") == pytest.approx(1305.106, 0.001)
+        assert geo_coder.location_coordinates("kolkata") == {
+            "lat": 22.5726459,
+            "lon": 88.3638953,
+        }
+        assert geo_coder.location_coordinates("delhi") == {
+            "lat": 28.6517178,
+            "lon": 77.2219388,
+        }
+        assert geo_coder.location_coordinates("PortaSigrar") == {
+            "lat": None,
+            "lon": None,
+        }
+
+    def test_location_distance(self, geo_coder: GeoCoder):
+        assert geo_coder.location_distance("kolkata", "delhi") == pytest.approx(
+            1305.106, 0.001
+        )
