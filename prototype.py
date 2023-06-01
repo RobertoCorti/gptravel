15b401326e945971a6a7ef8e79d65be14bb2d1fe
Roberto Corti <robi290596@gmail.com>
2023-06-01 16:26:44 +0200

    Refactor home page

--- prototype.py
+++ prototype.py
@@ -1,38 +1,51 @@
+import datetime
+
 import streamlit as st
-import airportsdata
+import pycountry
+import allcities
 
-airports = airportsdata.load()
+COUNTRIES = (country.name.lower() for country in pycountry.countries)
+CITIES = (city.name.lower() for city in allcities.cities)
 
 
-def travel_plan(airports, departure_airport, arrival_airport, departure_date, return_date, travel_reason, openai_key):
-    ## clean current page
-
-    st.empty()
+def travel_plan(departure, destination, departure_date, return_date, travel_reason):
+    st.markdown("TRAVEL PLAN ACTIVATED")
 
 
 def main():
     st.title("GPTravel")
     st.subheader("Your personal travel assistant")
 
-    st.sidebar.subheader("About")
-    about = st.sidebar.markdown("GPTravel is a personal travel assistant that helps you plan your next trip. "
-                                "It uses the power of OpenAI's GPT-3 to generate a travel plan based on your preferences. "
-                                "It is currently a prototype, so it is not perfect yet. ")
+    openai_key = st.sidebar.text_input("OpenAI API Key", placeholder="Enter your OpenAI key here")
 
-    openai_key = st.sidebar.text_input("OpenAI Key", placeholder="Enter your OpenAI key here")
+    departure_date = st.sidebar.date_input("Select a date")
+    return_date = st.sidebar.date_input(key="return_date", label="Select a return date")
+    departure = st.sidebar.text_input(label="Departure", placeholder="Select a departure")
+    destination = st.sidebar.text_input(label="Destination", placeholder="Select a destination")
 
-    departure_date = st.date_input("Select a date")
-    return_date = st.date_input(key="return_date", label="Select a return date")
+    travel_reason = st.sidebar.selectbox("Select a travel reason",
+                                         ["", "Business", "Romantic", "Solo", "Friends", "Family"])
 
-    airport_cities = sorted(set(f"{v['city']} ({v['country']})" for v in airports.values() if len(v['city']) != 0))
-    departure_airport = st.selectbox("Select a departure airport", airport_cities)
-    arrival_airport = st.selectbox("Enter an arrival airport", airport_cities)
+    input_options = dict(openai_key=openai_key, departure_date=departure_date, return_date=return_date,
+                         departure=departure, destination=destination, travel_reason=travel_reason)
 
-    travel_reason = st.selectbox("Select a travel reason", ["Business", "Romantic", "Solo", "Friends", "Family"])
+    if st.sidebar.button("Let's go!"):
+        if _is_valid_input(**input_options):
+            travel_plan(**input_options)
 
-    if st.button("Let's go!"):
-        travel_plan(airports, departure_airport, arrival_airport, departure_date, return_date, travel_reason,
-                    openai_key)
+
+def _is_valid_input(departure: str, destination: str,
+                    departure_date: datetime.datetime, return_date: datetime.datetime,
+                    openai_key: str, travel_reason: str) -> None:
+    if (departure.lower() not in CITIES) or (departure.lower() not in COUNTRIES) or (
+            destination.lower() not in CITIES) or (destination.lower() not in COUNTRIES):
+        st.sidebar.warning("Travel destination or/and departure is not valid.")
+    if departure_date >= return_date:
+        st.sidebar.warning("Travel dates are not correct. Departure should be before return.")
+    if openai_key == '':
+        st.sidebar.warning("Not valid OpenAI API Access Key")
+    else:
+        travel_plan(departure, destination, departure_date, return_date, travel_reason)
 
 
 if __name__ == "__main__":
